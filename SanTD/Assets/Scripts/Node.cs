06a88052436f4929d349dd5d64d1cf1dbb454587
Nygren Stefan <stefan.nygren@scania.com>
2019-02-12 15:40:48 +0100

    Add placement Highgliter alpha

---
+++ SanTD/Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    Material m_Material;
+    Material m_OrigMaterial;
+    public Material Replacement;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        m_Material = GetComponent<Renderer>().material;
+        m_OrigMaterial = GetComponent<Renderer>().material;
+    }
+    private void OnMouseEnter()
+    {
+        rend.material = Replacement; 
+    }
+    private void OnMouseExit()
+    {
+        rend.material = m_OrigMaterial;
+    }
+}
